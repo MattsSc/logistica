48e744b1c63605fea0554a973623659991799b31
matias.scandroglio <matias.scan@gmail.com>
2018-10-15 23:11:47 -0300

    add access-control

--- index.js
+++ index.js
@@ -55,8 +55,13 @@ app.use(session(sess));
 app.use(basePath + '/order', orderCtrl);
 app.use(basePath + '/delivery', deliveryCtrl);
 app.use(basePath + '/user', userCtrl);
-
 app.use('/', express.static(__dirname + '/'));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.listen(port, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', port, port);
