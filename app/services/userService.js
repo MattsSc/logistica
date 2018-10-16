217016a9234155131a5f5122121bb3029d2bd5cf
matias.scandroglio <matias.scan@gmail.com>
2018-10-15 23:25:29 -0300

    fix login error

--- app/services/userService.js
+++ app/services/userService.js
@@ -3,6 +3,8 @@ const dbUtils = require('../../utils/dbUtils.js');
 
 exports.loginUser = async (user) => {
     const userLogin = await dbUtils.findUser(user);
+    if(userLogin == null)
+        return userLogin;
     return userLogin._id;
 };
 
