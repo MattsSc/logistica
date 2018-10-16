362d68931aeb5f2bbf4c568caaf8e5c7df751a3a
matias.scandroglio <matias.scan@gmail.com>
2018-10-15 23:36:47 -0300

    fix login error

--- app/controllers/userCtrl.js
+++ app/controllers/userCtrl.js
@@ -5,6 +5,7 @@ const service = require('../services/userService.js');
 
 router.post('/login', async function (req, res) {
     try{
+        console.log("user is :" + JSON.stringify(req.body));
         const userId = await service.loginUser(req.body);
         if(userId == null)
             res.status(404).send("user not found");
--- app/services/userService.js
+++ app/services/userService.js
@@ -3,7 +3,7 @@ const dbUtils = require('../../utils/dbUtils.js');
 
 exports.loginUser = async (user) => {
     const userLogin = await dbUtils.findUser(user);
-    console.log(userLogin);
+    console.log("user" + userLogin);
     if(userLogin == null)
         return userLogin;
     return userLogin._id;
