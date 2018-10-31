159bcffeeceae8493ef74585c57be0f92479cdfa
matias.scandroglio <matias.scan@gmail.com>
2018-10-30 23:07:36 -0300

    change order

--- app/controllers/orderCtrl.js
+++ app/controllers/orderCtrl.js
@@ -4,51 +4,6 @@ const router = express.Router();
 const service = require('../services/orderService.js');
 const validator = require('../../utils/validators.js');
 
-router.get('/:orderId', async function (req, res) {
-    const orderId = req.params.orderId;
-    console.log("Se fue a buscar la orden con id " + orderId);
-    if(!validator.validateIsNumber(orderId))
-        res.status(400).send("El id es solo númerico!");
-    else{
-        try{
-            const doc = await service.findOrder(orderId);
-            res.send(doc);
-        }catch (e) {
-            res.sendStatus(404);
-        }
-    }
-});
-
-router.patch('/:orderId/complain', async function (req,res) {
-    const orderId = req.params.orderId;
-    console.log("Se informa queja a orden " + orderId);
-    if(!validator.validateIsNumber(orderId))
-        res.status(400).send("El id es solo númerico!");
-    else{
-        try{
-            const result = await service.informComplain(orderId);
-            res.sendStatus(200);
-        }catch (e) {
-           res.sendStatus(500);
-        }
-    }
-});
-
-router.patch('/:orderId', async function (req, res) {
-    const orderId = req.params.orderId;
-    console.log("Se actualiza al estado " + req.query.estado + " la orden " + orderId);
-    if(validator.validateIsNumber(orderId) && validator.validateBodyForUpdate(req.query.estado)) {
-        try{
-            const result = await service.updateStatus(req.params.orderId, req.query.estado);
-            res.sendStatus(200);
-        }catch (e) {
-            res.sendStatus(500);
-        }
-    }else{
-        res.status(404).send("Orden id debe ser númerico / El estado no pertenece a los que estan indicados : \"NEW\", \"RECEIVED\", \"ON_WAY\", \"DELIVERED\", \"COMPLETED\"")
-    }
-});
-
 router.get('/', async function (req, res) {
     try{
         const query = {
@@ -57,8 +12,6 @@ router.get('/', async function (req, res) {
 
         console.log("Se estan buscando las ordenes con estos filtros:" + JSON.stringify(query));
         const result = await service.getOrders(query.estado);
-
-        console.log("adadas " + result);
 
         res.send(result);
     }catch (e) {
@@ -81,5 +34,51 @@ router.post('/', async function (req, res) {
         res.sendStatus(500);
     }
 });
+
+router.get('/:orderId', async function (req, res) {
+    const orderId = req.params.orderId;
+    console.log("Se fue a buscar la orden con id " + orderId);
+    if(!validator.validateIsNumber(orderId))
+        res.status(400).send("El id es solo númerico!");
+    else{
+        try{
+            const doc = await service.findOrder(orderId);
+            res.send(doc);
+        }catch (e) {
+            res.sendStatus(404);
+        }
+    }
+});
+
+router.patch('/:orderId', async function (req, res) {
+    const orderId = req.params.orderId;
+    console.log("Se actualiza al estado " + req.query.estado + " la orden " + orderId);
+    if(validator.validateIsNumber(orderId) && validator.validateBodyForUpdate(req.query.estado)) {
+        try{
+            const result = await service.updateStatus(req.params.orderId, req.query.estado);
+            res.sendStatus(200);
+        }catch (e) {
+            res.sendStatus(500);
+        }
+    }else{
+        res.status(404).send("Orden id debe ser númerico / El estado no pertenece a los que estan indicados : \"NEW\", \"RECEIVED\", \"ON_WAY\", \"DELIVERED\", \"COMPLETED\"")
+    }
+});
+
+router.patch('/:orderId/complain', async function (req,res) {
+    const orderId = req.params.orderId;
+    console.log("Se informa queja a orden " + orderId);
+    if(!validator.validateIsNumber(orderId))
+        res.status(400).send("El id es solo númerico!");
+    else{
+        try{
+            const result = await service.informComplain(orderId);
+            res.sendStatus(200);
+        }catch (e) {
+           res.sendStatus(500);
+        }
+    }
+});
+
 
 module.exports = router;
