091e8d9de440d25abdf4ac3606de5a7da57e3e51
matias.scandroglio <matias.scan@gmail.com>
2018-10-30 23:21:38 -0300

    add cors dependency

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const swaggerTools = require('swagger-tools');
 const jsyaml = require('js-yaml');
 
@@ -50,24 +51,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 app.use(session(sess));
+app.use(cors());
 
 app.use(function errorHandler (err, req, res, next) {
-
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-
     if (req.xhr) {
         res.status(500).send({ error: 'Something failed!' })
     } else {
